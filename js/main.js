ff4c5b6d77e164987baa6b056b744a108b1447ae
AngelaJiaNan <angelanan12@gmail.com>
2021-10-26 18:35:12 -0700

    I have completed create-entry-form-task1.

--- js/main.js
+++ js/main.js
@@ -1,2 +1,27 @@
 /* global data */
 /* exported data */
+var url = document.querySelector('#user-url');
+var uploadedPicture = document.querySelector('#placeholder');
+
+url.addEventListener('input', function (event) {
+  var pictureUrl = event.target.value;
+  uploadedPicture.setAttribute('src', pictureUrl);
+});
+
+var entry = document.querySelector('#code-journal');
+var form = document.forms[0];
+
+entry.addEventListener('submit', noDefault);
+
+function noDefault(event) {
+  event.preventDefault();
+  var object = {};
+  object.title = form.elements[0].value;
+  object.url = form.elements[1].value;
+  object.notes = form.elements[2].value;
+  object.entryId = data.nextEntryId;
+  data.nextEntryId += 1;
+  data.entries.push(object);
+  uploadedPicture.src = 'images/placeholder-image-square.jpg';
+  form.reset();
+}
